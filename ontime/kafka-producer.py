87f17c4e22ba11d50dc0c41fdc4db54af686ca11
Paolo Cozzi <paolo.cozzi@ptp.it>
2016-02-15 12:28:57 +0100

    kafka-producer.py rewritten
    Now kafka-producer is able to scan directory recursively

--- ontime/kafka-producer.py
+++ ontime/kafka-producer.py
@@ -12,19 +12,15 @@ import os
 import sys
 import time
 import logging
+import argparse
 
 from kafka.common import LeaderNotAvailableError, UnknownError, KafkaTimeoutError
 from kafka import KafkaProducer
 
-from datetime import datetime
-
-from common import *
-
 # Pydoop
 import pydoop.hdfs as hdfs
 
-#get file list
-test_dataset = hdfs.ls(TEST_DIR)
+from common import HOST
 
 # a Global variable
 producer = None
@@ -41,14 +37,14 @@ def print_response(record_metadata=None):
         msg = "; ".join(msg)
         logger.debug(msg)
 
-def submit(topic, data, trials=3, async=True):
+def submit(topic, line, trials=3, async=True):
     step = 0
     global producer
     
     while step < trials:
         step += 1
         try:
-            future = producer.send(topic, "\n".join(data))
+            future = producer.send(topic, line)
             
             if async is False:
                 record_metadata = future.get(timeout=10)
@@ -62,51 +58,70 @@ def submit(topic, data, trials=3, async=True):
             time.sleep(10)
             
     #If arrive here
-    logger.warn("data %s ignored" %(data))
+    logger.warn("line %s ignored" %(line))
     return #anyway
 
-def main():
+def main(directory, topic):
+    #get a hdfs object
+    myHdfs = hdfs.hdfs()
+    myPath = myHdfs.walk(directory)
+    
     # a global variable
     global producer 
 
     producer = KafkaProducer(bootstrap_servers=["%s:6667" %(HOST)], compression_type='gzip', acks=1, retries=2)
     
-    for myfile in test_dataset:
-        if "_SUCCESS" in myfile:
+    for myfile in myPath:
+        #Skip directory recursive
+        if myfile["kind"] == "directory":
+            logger.debug("ignoring %s" %(myfile))
             continue
         
-        logger.info("Working on %s" %(myfile))
-        with hdfs.open(myfile) as handle:
-            data = []
+        elif myfile["kind"] == "file":
+            pass
+        
+        else:
+            raise Exception, "Unknown kind %s for %s" %(myfile["kind"], myfile["name"])
+            
+        #Skip name in particoular
+        if "_SUCCESS" in myfile["name"] or "_temporary" in myfile["name"]:
+            logger.debug("ignoring %s" %(myfile))
+            continue
+        
+        #Skip 0 size files
+        if myfile["size"] == 0:
+            logger.debug("ignoring %s" %(myfile))
+            continue
+        
+        logger.info("Working on %s" %(myfile["name"]))
+
+        with hdfs.open(myfile["name"]) as handle:
             for i, line in enumerate(handle):
                 #strip line
                 line = line.strip()
                 
-                data += [line]
+                submit(topic, line, trials=3)
                 
-                if i % 5000 == 0:                                 
-                    #Submit data (my function)
-                    submit(TOPIC, data, trials=3)
-                    data = []
-                
-                if i % 20000 == 0:
-                    logger.info("%s lines submitted for %s" %(i, myfile))
+                if i % 20000 == 0 and i != 0:
+                    logger.info("%s lines submitted for %s" %(i, myfile["name"]))
                     
             #for every line
             
-            #submit the rest of the data
-            submit(TOPIC, data, trials=3)
-            data = []
-            
         #with file open
-        logger.info("Completed %s" %(myfile))
+        logger.info("Completed %s" %(myfile["name"]))
         
         #sleep some time
-        time.sleep(5)
+        time.sleep(1)
                     
     # for all files in HDFS
     producer.close()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Scan a directory and upload data in kafka-topic')
+    parser.add_argument('-d', '--directory', type=str, required=True, help='The HDFS directory file')
+    parser.add_argument('-t', '--topic', type=str, required=True, help="The kafka topic")
+    args = parser.parse_args()
+        
+    #call main function
+    main(directory=args.directory, topic=args.topic)
 
